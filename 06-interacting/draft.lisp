7ae5e587c76c53f025db766e0e94c3c82d6ca8fd
pietrodito <pietrodito@gmail.com>
2019-07-30 16:37:35 +0200

    Chapter 6 finished

---
+++ 06-interacting/draft.lisp
@@ -0,0 +1,17 @@
+(defun say-hello ()
+  (print "Please type your name:")
+  (let ((name (read)))
+    (print "Nice to meet you, ")
+    (print name)))
+
+(defun add-five ()
+  (print "Enter a number:")
+  (let ((num (read)))
+    (print "Adding five to your number")
+    (print (+ 5 num))))
+
+(defun say-hello ()
+  (princ "Please type your name:")
+  (let ((name (read-line)))
+    (princ "Nice to meet you, ")
+    (princ name)))
---
+++ 06-interacting/test-curry.lisp
@@ -0,0 +1,2 @@
+(defun my-print ()
+  (print))
