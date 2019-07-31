856a48c2b13f9469d4d51798485e5de20a2c5267
pietrodito <pietrodito@gmail.com>
2019-07-31 20:34:45 +0200

    CH7 I/O issues with sbcl

---
+++ 07-lists/draft.lisp
@@ -0,0 +1,12 @@
+(setf *print-circle* t)
+(defparameter foo '(1 2 3))
+(setf (cdddr foo) foo)
+(print foo)
+
+(defparameter *drink-order* '((bill . double-espresso)
+                              (lisa . small-drip-coffee)
+                              (john . medium-latte)))
+
+(assoc 'lisa *drink-order*)
+(push '(lisa . large-mocha-with-whipped-cream) *drink-order*)
+(assoc 'lisa *drink-order*)
